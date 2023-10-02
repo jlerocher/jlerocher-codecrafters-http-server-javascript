03220fb133471ff8cd87bbc5e4bc115d233c00a8
Jean Eliézer Le Rocher <jlerocher@gmail.com>
2023-10-02 19:30:46 +0200

    pass 5th stage

--- app/main.js
+++ app/main.js
@@ -1,6 +1,13 @@
 const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+function createResponse(status, headers, body) {
+    let headerStr = ""
+    for(const header in headers) {
+        headerStr += header + ": " + headers[header] + "\r\n"
+    }
+    return `HTTP/1.1 ${status}\r\n${headerStr}Content-Length: ${new Blob([body]).size}\r\n\r\n${body}`;
+}
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("close", () => {
@@ -11,7 +18,12 @@ const server = net.createServer((socket) => {
         const lines = data.toString().split("\r\n")
         const [method, path, protocol] = lines[0].split(" ")
         let section = 0;
-        const headers = lines.filter((v, i) => i >= 2 && v != "").map(l => l.split(": "))
+        const headersRaw = lines.filter((v, i) => i >= 2 && v != "").map(l => l.split(": "))
+        let headers = {}
+        for(const header of headersRaw) {
+            headers[header[0]] = header[1]
+1
+        }
         console.log("method", method)
         console.log("path", path)
         console.log("protocol", protocol)
@@ -20,13 +32,17 @@ const server = net.createServer((socket) => {
             socket.write("HTTP/1.1 200 OK\r\n\r\n")
         } else if(path.startsWith("/echo/")) {
             const garbage = path.replace(/^\/echo\//g, "")
-            
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${new Blob([garbage]).size}\r\n\r\n${garbage}`)
+            socket.write(createResponse("200 OK", {
+                "Content-Type": "text/plain"
+            }, garbage))
+        } else if(path.startsWith("/user-agent")) {
+            socket.write(createResponse("200 OK", {
+                "Content-Type": "text/plain"
+1
+            }, headers["User-Agent"]))
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
-1
         }
-1
         socket.end();
     })
 });
